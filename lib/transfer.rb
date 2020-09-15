e1e68d40a6108f9217653f61e42328ef6e964c1c
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 02:10:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,24 +9,23 @@ class Transfer
     @status = "pending"
   end
   def valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
+   if sender.valid? && receiver.valid?
+     true
+   else
+     false
+  end
   end
   def execute_transaction
-     if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
+   if @sender.balance < @amount
+     @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  elsif @status == "complete"
+    puts "Transaction was already excuted"
+  else
+    @sender.deposit( @amount * -1 ) 
+    @receiver.deposit( @amount )
+     @status = "complete"
+  end
   end
 
 
