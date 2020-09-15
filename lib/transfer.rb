1978b1c2d9b4ffe80eaa5dfcd07056e2e82c34e4
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 02:06:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,13 @@
 class Transfer
   
+  attr_accessor :sender, :receiver, :amount, :status, :count
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
   
 end
